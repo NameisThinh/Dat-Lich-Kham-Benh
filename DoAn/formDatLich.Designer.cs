df34d510d071161681f740a75f358437e62a19b5
Nameisthinh <quocthinh00123@gmail.com>
2022-10-01 22:54:09 +0700

    Update form đặt lịch

--- DoAn/formDatLich.Designer.cs
+++ DoAn/formDatLich.Designer.cs
@@ -28,15 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // formDatLich
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(1439, 880);
+            this.ClientSize = new System.Drawing.Size(959, 563);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "formDatLich";
             this.Text = "formDatLich";
             this.ResumeLayout(false);
@@ -44,5 +56,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
