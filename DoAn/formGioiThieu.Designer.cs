e43530c24b05336903946a1cbe966a789176d1e2
Trần Phúc Hiếu <phuchieu20.01@gmail.com>
2022-10-10 03:55:41 +0700

    ...

--- DoAn/formGioiThieu.Designer.cs
+++ DoAn/formGioiThieu.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(358, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // formGioiThieu
             // 
@@ -37,6 +47,7 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(1151, 745);
+            this.Controls.Add(this.button1);
             this.Name = "formGioiThieu";
             this.Text = "Giới thiệu";
             this.ResumeLayout(false);
@@ -44,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
